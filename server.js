5b26959d35f9c74a088b0df85c12322660441bdc
unknown <bilawal-cyber>
2021-10-18 02:40:24 -0700

    change env port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./index')
-
-app.listen(process.env.SERVER_PORT || 5000, (err) => {
+let port =process.env.PORT || 5000
+app.listen(port, (err) => {
     if (err) throw err
-    console.log(`Server is running on http://127.0.0.1:5000`)
+    console.log(`Server is running on http://127.0.0.1:${port}`)
 })
